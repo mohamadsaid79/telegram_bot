e4416f621ac09780ef886943157675bcd09142b6
mohamadsaid79 <109232942+mohamadsaid79@users.noreply.github.com>
2025-08-07 14:33:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-echo "✅ البوت شغال تمام!";
+$botToken = '8093702414:AAH8rhE2HKCaHtwaPPPDea7JDU5T_mYvgoc';
+$apiURL = "https://api.telegram.org/bot$botToken/";
+
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+
+if (isset($update['message']['chat']['id'])) {
+    $chat_id = $update['message']['chat']['id'];
+    $url = $apiURL . "sendMessage";
+    $data = [
+        'chat_id' => $chat_id,
+        'text' => "✅ البوت يعمل! أرسل أي رسالة وسيظهر لك رد."
+    ];
+    file_get_contents($url . '?' . http_build_query($data));
+}
+?>
